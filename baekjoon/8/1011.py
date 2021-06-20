ab02d47c737e2fd4d9d6423bb447f60fb1824170
Moonster <msj5589@gmail.com>
2021-06-20 22:07:46 +0900

    practice 0620

---
+++ baekjoon/8/1011.py
@@ -0,0 +1,37 @@
+for _ in range(int(input())):
+    , b = map(int, input().split(' '))
+    print(_)
+
+
+
+diff_list = [1] * diff
+diff = b - a
+p = diff // 2
+
+forward = p * (p + 1) // 2
+
+diff - forward
+
+k ## 총 이동 횟수
+a ## 증가하는 횟수
+## 일정한 횟수
+k - a ## 감소하는 횟수
+
+
+
+k + a
+k - a
+[1, 1, 1, 1, 1, 1, 1, 1, 1]
+
+
+
+
+1 1  # 2 
+2 1  # 3
+2 1 1 # 4
+2 2 1 # 5
+4 3 2 1 # 6
+5 4 3 2 1
+
+1 2 1 1 
+
