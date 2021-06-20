b25a88d834cafc215657dcf2409e62c7546a7849
Moonster <msj5589@gmail.com>
2021-06-20 22:10:47 +0900

    Update 1011.py

--- baekjoon/8/1011.py
+++ baekjoon/8/1011.py
@@ -1,16 +1,11 @@
 for _ in range(int(input())):
-    , b = map(int, input().split(' '))
+    start, end = map(int, input().split(' '))
     print(_)
 
 
 
-diff_list = [1] * diff
-diff = b - a
-p = diff // 2
+diff = end - start
 
-forward = p * (p + 1) // 2
-
-diff - forward
 
 k ## 총 이동 횟수
 a ## 증가하는 횟수
@@ -18,6 +13,18 @@ a ## 증가하는 횟수
 k - a ## 감소하는 횟수
 
 
+
+
+1 * k + a * k + (k - a) = diff
+
+
+k = 3
+a = 2
+
+
+
+
+(a + 2) * k - a = diff
 
 k + a
 k - a
